16cbbd9c6891bff67766e40e21057f3faceceea5
gopikaanilam2004-stack <gopikaanilam2004@gmail.com>
2026-07-05 19:53:14 +0530

    Initial Spring Boot project

---
+++ src/test/java/com/gopika/employee_api/EmployeeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gopika.employee_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
